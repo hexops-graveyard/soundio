e825cb7dd3392787a514f876057a88245804f2e6
alichraghi <alichraghi@pm.me>
2022-06-26 13:09:21 +0430

    add soundio

---
+++ update-upstream.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -e -x
+
+rm -rf soundio
+git clone --depth 1 https://github.com/andrewrk/libsoundio soundio
+
+pushd soundio
+git reset --hard 8ab36069123a8b38e247ace55f8a6b3e6ee14f5f
+soundio_delete=($(find -maxdepth 1 | grep -v -E '^./(soundio|src|LICENSE)$'))
+rm -rf "${soundio_delete[@]}" || true
+rm -rf "src/config.h.in"
+popd
